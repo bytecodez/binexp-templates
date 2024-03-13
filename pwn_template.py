b4c5461d02c5a6961471977d00d94084e030eb96
pwnpope <108923813+pwnpope@users.noreply.github.com>
2024-03-13 17:07:41 +0000

    Create pwn_template.py

---
+++ pwn_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from pwn import *
+
+elf = context.binary = ELF("targ", checksec=True)
+libc = elf.libc
+context.log_level = "debug"
+#context.log_level = "critical"  # quiet mode
+
+gs = """
+continue
+"""
+
+def start():
+    if args.GDB:
+        return gdb.debug(elf.path, gdbscript=gs)
+    elif args.REMOTE():
+      remote("1.1.1.1", 5555) 
+    else:
+        return process(elf.path)
+
+
+io = start()
